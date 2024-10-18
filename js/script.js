56ed63f36f002452e90b4294f9de811b004b334b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-10-17 22:39:48 -0300

    finalizado

---
+++ js/script.js
@@ -0,0 +1,49 @@
+let alunos = [];
+
+function AddAluno(){
+
+aluno = {
+
+    "nome": document.getElementById("nome").value,
+    "nasc": document.getElementById("nasc").value,
+    "email": document.getElementById("email").value,
+    "curso": document.getElementById("curso").value
+}
+
+alunos.push(aluno);
+LimparCampos();
+
+}
+
+function ViewAlunos(){
+console.log(alunos);
+}
+
+function LimparCampos(){
+
+    document.getElementById("nome").value = "";
+    document.getElementById("nasc").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("curso").value = "";
+
+}
+
+function Viewname(){
+
+    for(let i = 0; i < alunos.length; i++){
+
+        console.log(alunos[i].nome);
+    }
+
+
+} 
+
+function UpdateCursos(){
+
+    for(let i = 0; i < alunos.length; i++){
+
+        alunos[i].curso = "Informática";
+    }
+
+}
+
